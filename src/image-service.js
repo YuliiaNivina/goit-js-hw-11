fc75a0b0f97a3226ddb040f5b2482ad8e6b05ba6
Yuliia Nivina <y.nivina@gmail.com>
2023-03-28 00:10:55 +0300

    Add gallery

---
+++ src/image-service.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+const API_KEY = '34552003-c041c4010936caa6c4fdbe25f';
+const BASE_URL = `https://pixabay.com/api`;
+
+
+export default class GetImagesService {
+  constructor() {
+    this.searchQuery = '';
+    this.page = 1;
+    this.allImages = 0;
+    this.totalImages = 0;
+  }
+
+  async getImages() {
+    const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
+
+    try {
+      console.log(this);
+      const response = await axios.get(url);
+      const images = await response.data;
+
+      this.allImages += images.hits.length;
+      this.totalImages = images.totalHits;
+      // const nextimages = await (this.page += 1);
+      console.log(images);
+      console.log(this.allImages);
+      console.log(this.totalImages);
+
+      return images;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  addPage() {
+    this.page += 1;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  resetAllImages() {
+    this.allImages = 0;
+  }
+
+  // get query() {
+  //   return this.searchQuery;
+  // }
+
+  // set query(newQuery) {
+  //   this.searchQuery = newQuery;
+  // }
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,166 @@
 import Notiflix from 'notiflix';
+import GetImagesService from './image-service';
 import './css/styles.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const refs = {
+  searchForm: document.querySelector('#search-form'),
+  pictureContainer: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more'),
+  imagesGallery: document.querySelector('.gallery'),
+};
+
+const getImagesService = new GetImagesService();
+
+refs.searchForm.addEventListener('submit', onFormSearch);
+refs.loadMoreBtn.addEventListener('click', onLoadMore);
+
+function onFormSearch(evt) {
+  evt.preventDefault();
+  refs.loadMoreBtn.disabled = false;
+
+  getImagesService.searchQuery = evt.currentTarget.elements.searchQuery.value;
+
+  if (getImagesService.searchQuery === '') {
+    refs.loadMoreBtn.classList.add('is-hidden');
+    clearGalleryMarkup();
+    return Notiflix.Notify.warning(`Enter a search name, please!`);
+  }
+
+  clearGalleryMarkup();
+  
+  refs.loadMoreBtn.classList.add('is-hidden');
+  getImagesService.resetPage();
+  getImagesService.resetAllImages();
+  refs.loadMoreBtn.disabled = false;
+  refs.loadMoreBtn.textContent = 'Load more';
+
+  getImagesService.getImages().then(images => {
+    clearGalleryMarkup();
+    addGalleryMarkup(images);
+
+    if (images.hits.length === 0) {
+      refs.loadMoreBtn.classList.add('is-hidden');
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    } else {
+      refs.loadMoreBtn.classList.remove('is-hidden');
+      Notiflix.Notify.info(`Hooray! We found ${images.totalHits} images.`);
+    }
+
+    simpleLightbox();
+  });
+}
+
+async function onLoadMore() {
+  getImagesService.addPage();
+
+  await getImagesService.getImages().then(images => {
+    addGalleryMarkup(images);
+
+    simpleLightbox();
+    smoothScroll();
+
+    if (getImagesService.allImages === getImagesService.totalImages) {
+      refs.loadMoreBtn.disabled = true;
+      refs.loadMoreBtn.textContent = 'End of content';
+    }
+  });
+}
+
+function createImageMarkup(images) {
+  return images.hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `
+          <div class="photo-card" data-infinite-scroll='{ "path": ".pagination__next", "append": ".post", "history": false }'>
+            <div class="img-container">
+              <a class="img-link" href="${largeImageURL}">
+                <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+              </a>
+            </div>
+            <div class="info">
+              <p class="info-item">
+                <b>Likes</b>${likes}
+              </p>
+              <p class="info-item">
+                <b>Views</b>${views}
+              </p>
+              <p class="info-item">
+                <b>Comments</b>${comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads</b>${downloads}
+              </p>
+            </div>
+          </div>`;
+      }
+    )
+    .join('');
+}
+
+function addGalleryMarkup(images) {
+  refs.imagesGallery.insertAdjacentHTML('beforeend', createImageMarkup(images));
+}
+
+function clearGalleryMarkup() {
+  refs.imagesGallery.innerHTML = '';
+}
+
+function simpleLightbox() {
+  var lightbox = new SimpleLightbox('.img-link', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  }).refresh();
+}
+
+function smoothScroll() {
+  const { height: cardHeight } = document
+    .querySelector('.gallery')
+    .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
+
+// async function checkPosition() {
+//   // Нам потребуется знать высоту документа и высоту экрана:
+//   const height = document.body.offsetHeight;
+//   const screenHeight = window.innerHeight;
+
+//   // Они могут отличаться: если на странице много контента,
+//   // высота документа будет больше высоты экрана (отсюда и скролл).
+
+//   // Записываем, сколько пикселей пользователь уже проскроллил:
+//   const scrolled = window.scrollY;
+
+//   // Обозначим порог, по приближении к которому
+//   // будем вызывать какое-то действие.
+//   // В нашем случае — четверть экрана до конца страницы:
+//   const threshold = height - screenHeight / 4;
+
+//   // Отслеживаем, где находится низ экрана относительно страницы:
+//   const position = scrolled + screenHeight;
+
+//   if (position >= threshold) {
+//     // Если мы пересекли полосу-порог, вызываем нужное действие.
+//    onLoadMore();
+//   }
+
+// }
+// (() => {
+//   window.addEventListener('scroll', checkPosition);
+//   // window.addEventListener('resize', checkPosition);
+// })();
